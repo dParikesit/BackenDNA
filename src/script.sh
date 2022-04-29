42bdca69d591988e9e20634cff00fc7e9937bb85
Dimas Shidqi Parikesit <dsparikesit@gmail.com>
2022-04-29 13:23:21 +0700

    update bug

---
+++ src/script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd frontend
+
+echo "Installing frontend dependencies"
+npm install
+
+echo "Building frontend"
+npm run build
+rm -rf ../backend/frontend
+mkdir ../backend/frontend
+mv build ../backend/frontend/build
+
+cd ../backend
+
+echo "Installing backend dependencies"
+go mod download
+go run main.go
